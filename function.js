493a80ec5d44ee1f43c3a66fdf5f6e5bebc5df80
pbvansh <vanshpratik0165@gmail.com>
2023-01-05 19:05:56 +0530

    done with functions

---
+++ function.js
@@ -0,0 +1,15 @@
+function addToNum(a,b){
+    return a+b;
+}
+
+const addToNum2 = function(a,b){
+    return a+b;
+}
+
+const addToNum3 = (a,b)=>{
+    return a+b;
+}
+
+console.log(addToNum(10,20)); //30
+console.log(addToNum2(10,20)); //30
+console.log(addToNum3(10,20)); //30
