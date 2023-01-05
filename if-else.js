1d4cdf4cea915bc546693e96fcb23061c0a6d5d4
pbvansh <vanshpratik0165@gmail.com>
2023-01-05 13:07:06 +0530

    initial commit

---
+++ if-else.js
@@ -0,0 +1,10 @@
+
+const email = "pratik@gmail.com";
+const password = "Pratik@123";
+
+if(email === "pratik@gmail.com" && password==="Pratik@123"){
+    console.log('login successfully');
+}
+else{
+    console.log('Please provide valid email or password');
+}
---
+++ switch.js
@@ -0,0 +1,22 @@
+const a = 10;
+const b =20;
+
+const opetion = 4;
+
+switch (opetion) {
+    case 1:
+        console.log(a+b);
+        break;
+    case 2:
+        console.log(a-b);
+        break;
+    case 3:
+        console.log(a*b);
+        break;
+    case 4:
+        console.log(a/b);
+        break;
+
+    default:
+        break;
+}
