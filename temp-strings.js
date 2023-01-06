40b5e30b90d24d6eadf453bd7aee78021211cb99
pbvansh <vanshpratik0165@gmail.com>
2023-01-06 17:56:03 +0530

    done with template string

---
+++ temp-strings.js
@@ -0,0 +1,8 @@
+const name = 'pratik';
+const age = 20;
+ 
+// console.log(name.charAt(0).toUpperCase() + name.slice(1));
+
+console.log(`${name} is ${age} year old`); //pratik is 20 year old
+console.log(`${name} is ${age+2} year old`);  //pratik is 22 year old
+console.log(`${name.toUpperCase()} is ${age+2} year old`);  //PRATIK is 22 year old
